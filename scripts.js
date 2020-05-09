35ba25415f8566118a83fbc61f233fee766bfefa
Emily Dardis <63334996+EmilyGitHub@users.noreply.github.com>
2020-05-09 07:42:18 -0700

    added list and coming soon page links

---
+++ scripts.js
@@ -0,0 +1,41 @@
+$("#signInButton").click(function() {
+    $("#loginModal").modal();
+});
+
+$("#registerButton").click(function() {
+    $("#registerModal").modal();
+});
+
+function addTodo() {
+    let textbox = document.getElementById("todo-textbox");
+    if(textbox.value == "")
+      return;
+    
+    let li = document.createElement("li");
+    let todoText = document.createTextNode(textbox.value);
+    li.appendChild(todoText);
+    
+    let deleteButton = document.createElement("button");
+    let deleteText = document.createTextNode("x");
+    deleteButton.appendChild(deleteText);
+    deleteButton.onclick = deleteTodo;
+    li.appendChild(deleteButton);
+    
+    let ul = document.getElementById("todo-list");
+    ul.appendChild(li);
+    
+    textbox.value = "";
+};
+  
+function deleteTodo() {
+    let li = this.parentElement;
+    li.parentElement.removeChild(li);
+};
+
+let textbox = document.getElementById("todo-textbox");
+textbox.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("todo-add").click();
+  }
+});
